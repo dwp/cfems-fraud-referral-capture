64804b4baeab76253b1e77ccf6898f866fb8fcfa
StuBamforth <stuart.n.bamforth@engineering.digital.dwp.gov.uk>
2025-12-12 11:32:40 +0000

    First push of v2
    GitOrigin-RevId: fc7f3129acbd10928def0e025a98fe4d442162f4

--- app/routes.js
+++ app/routes.js
@@ -35,3 +35,32 @@ router.post('/prototype1/new-information', (req, res) => {
     res.redirect('dropout')
     }   
 });
+
+
+// Prototype 2 - Country redirect
+router.post('/prototype2/country', (req, res) => {
+        if(req.session.data['country-check'] == 'scotland'){
+    res.redirect('scotland')
+        } else {
+    res.redirect('reported-before')
+    }   
+});
+
+// Prototype 2 - Reported them before check
+router.post('/prototype2/reported-before', (req, res) => {
+        if(req.session.data['reported-before-check'] == 'Yes'){
+    res.redirect('new-information')
+        } else {
+    res.redirect('details')
+    }   
+});
+
+// Prototype 2 - New information check
+router.post('/prototype2/new-information', (req, res) => {
+        if(req.session.data['new-information-check'] == 'Yes'){
+    res.redirect('details')
+        } else {
+    res.redirect('dropout')
+    }   
+});
+
