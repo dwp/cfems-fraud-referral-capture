9056714a6a371f85e009674a47b1a913499af8e7
StuBamforth <stuart.n.bamforth@engineering.digital.dwp.gov.uk>
2026-02-27 15:55:32 +0000

    Migrated all new work from from old prototype
    GitOrigin-RevId: 84b90cfd48ac458695e3cd831dfaa4a24c96cda4

--- app/routes.js
+++ app/routes.js
@@ -39,7 +39,7 @@ router.post('/prototype1/new-information', (req, res) => {
 
 // Prototype 2 - Country redirect
 router.post('/prototype2/country', (req, res) => {
-        if(req.session.data['country-check'] == 'scotland'){
+        if(req.session.data['country-check'] == 'Scotland'){
     res.redirect('scotland')
         } else {
     res.redirect('reported-before')
@@ -72,3 +72,41 @@ router.post('/prototype2/address-known', (req, res) => {
     res.redirect('fraud-type')
     }   
 });
+
+// Prototype 2 - Partner address branch
+router.post('/prototype2/partner-address-known', (req, res) => {
+        if(req.session.data['partner-address-known'] == 'Yes, I know the address'){
+    res.redirect('partner-address-search')
+        } else {
+    res.redirect('partner-working-branch')
+    }   
+});
+
+// Prototype 2 - Partner address branch
+router.post('/prototype2/partner-working', (req, res) => {
+        if(req.session.data['partner-working'] == 'Yes'){
+    res.redirect('living-with-partner-work')
+        } else {
+    res.redirect('additional-information-living-with-partner')
+    }   
+});
+
+
+// Prototype 2 - Fraud type checkboxes redirects
+router.post('/prototype2/fraud-types-check', function(request, response) {
+
+	var fraudtypes = request.session.data['fraudtypes']
+	if (fraudtypes.includes("working-claiming")){
+		response.redirect("working-and-claiming-benefits")
+	} 
+    else if (fraudtypes.includes("living-with-partner")){
+		response.redirect("living-with-partner-about-partner")
+    }
+    else if (fraudtypes.includes("disability")){
+		response.redirect("disability")
+    }
+    else {
+		response.redirect("fraud-type")
+	}
+})
+
